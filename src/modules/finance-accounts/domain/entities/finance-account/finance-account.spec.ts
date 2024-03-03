2fc2fb3764ebf455453fa113eab35367bbee6e30
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-03 12:18:53 -0300

    feat: FinanceAccount - implemantation

--- src/modules/finance-accounts/domain/entities/finance-account/finance-account.spec.ts
+++ src/modules/finance-accounts/domain/entities/finance-account/finance-account.spec.ts
@@ -3,29 +3,82 @@ import {
   FinanceAccountFactory,
   FinanceAccountProps,
 } from '@finance-accounts/domain/entities';
+import { BadRequestError } from '@shared/domain/errors';
 
 describe('FinanceAccount unit tests', () => {
   const data: FinanceAccountProps = {
     id: randomUUID(),
     name: 'test',
-    users: [],
+    users: [randomUUID()],
     date: new Date().toISOString(),
   };
 
   it('should create a FinanceAccount correctly', () => {
     const financeAccount = FinanceAccountFactory.create(data);
-
     const result = financeAccount.toJSON();
 
     expect(financeAccount.id).toStrictEqual(data.id);
     expect(financeAccount.name).toStrictEqual('test');
-    expect(financeAccount.users).toStrictEqual([]);
+    expect(financeAccount.users).toStrictEqual(data.users);
     expect(financeAccount.date).toStrictEqual(data.date);
     expect(result).toStrictEqual({
       id: data.id,
       name: 'test',
-      users: [],
+      users: data.users,
       date: data.date,
+    });
+  });
+
+  describe('validation', () => {
+    it('should throw a BadRequestError if id is invalid', () => {
+      const testData = { ...data, id: 'dfcdcfd' };
+
+      expect(() => FinanceAccountFactory.create(testData)).toThrow(
+        new BadRequestError('id in invalid format'),
+      );
+    });
+
+    it('should throw a BadRequestError if one of users is invalid', () => {
+      const testData = {
+        ...data,
+        id: null,
+        date: null,
+        users: ['dfcdcfd', randomUUID()],
+      };
+
+      expect(() => FinanceAccountFactory.create(testData)).toThrow(
+        new BadRequestError('users in invalid format'),
+      );
+    });
+
+    it('should throw a BadRequestError if users list is empty', () => {
+      const testData = { ...data, id: null, users: null };
+
+      expect(() => FinanceAccountFactory.create(testData)).toThrow(
+        new BadRequestError('users can not be empty'),
+      );
+    });
+
+    it('should throw a BadRequestError if date is invalid', () => {
+      const testData = { ...data, date: 'dfcdcfd' };
+
+      expect(() => FinanceAccountFactory.create(testData)).toThrow(
+        new BadRequestError('date in invalid format'),
+      );
+    });
+
+    it('should throw a BadRequestError if name is in incorrect lenth', () => {
+      let testData = { ...data, name: 'd' };
+
+      expect(() => FinanceAccountFactory.create(testData)).toThrow(
+        new BadRequestError('name must contain at least 2 characters'),
+      );
+
+      testData = { ...data, name: 'dasdfasdfs1'.repeat(10) };
+
+      expect(() => FinanceAccountFactory.create(testData)).toThrow(
+        new BadRequestError('name must contain a maximum of 100 characters'),
+      );
     });
   });
 });
--- src/modules/finance-accounts/domain/entities/finance-account/finance-account.ts
+++ src/modules/finance-accounts/domain/entities/finance-account/finance-account.ts
@@ -1,3 +1,13 @@
+import { BadRequestError } from '@shared/domain/errors';
+import {
+  ISODateValidation,
+  MaxLengthFieldValidation,
+  MinLengthFieldValidation,
+  UUIDValidation,
+  Validation,
+  ValidationComposite,
+  Validator,
+} from '@shared/domain/validations';
 import { randomUUID } from 'node:crypto';
 
 export type FinanceAccountProps = {
@@ -25,6 +35,7 @@ class FinanceAccount implements IFinanceAccount {
     this._name = name;
     this._users = users || [];
     this._date = date || new Date().toISOString();
+    this.validation();
   }
 
   get id(): string {
@@ -50,6 +61,35 @@ class FinanceAccount implements IFinanceAccount {
       date: this._date,
       users: this._users,
     };
+  }
+
+  private validation(): void {
+    const validator = this.createValidator();
+    validator.validate(this.toJSON());
+
+    if (this._users.length <= 0) {
+      throw new BadRequestError('users can not be empty');
+    }
+
+    this._users.forEach((id) => {
+      const isNotCorrectUUID = !Validator.isUUID(id);
+      if (isNotCorrectUUID) {
+        throw new BadRequestError('users in invalid format');
+      }
+    });
+  }
+
+  private createValidator(): Validation {
+    const validations: Validation<FinanceAccountProps>[] = [
+      new UUIDValidation('id'),
+
+      new MinLengthFieldValidation('name', 2),
+      new MaxLengthFieldValidation('name', 100),
+
+      new ISODateValidation('date'),
+    ];
+
+    return new ValidationComposite(validations);
   }
 }
 
