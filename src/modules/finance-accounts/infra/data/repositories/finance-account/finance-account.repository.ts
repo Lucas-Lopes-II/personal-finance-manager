75b1acd870cf84558f34da1fefe52e85def97569
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-05 19:57:13 -0300

    feat: FinanceAccountRepository - addUserInAccount method implementation

--- src/modules/finance-accounts/infra/data/repositories/finance-account/finance-account.repository.ts
+++ src/modules/finance-accounts/infra/data/repositories/finance-account/finance-account.repository.ts
@@ -1,12 +1,16 @@
 import { randomUUID } from 'node:crypto';
 import { DataSource, Repository } from 'typeorm';
-import { FinanceAccountEntity } from '@finance-accounts/infra/data/entities';
+import {
+  FinanceAccountEntity,
+  FinanceAccountUserEntity,
+} from '@finance-accounts/infra/data/entities';
 import { IFinanceAccountRepository } from '@finance-accounts/domain/repositories';
 import { FinanceAccountProps } from '@finance-accounts/domain/entities';
 
 export class FinanceAccountRepository implements IFinanceAccountRepository {
   public static instance: FinanceAccountRepository | null = null;
-  public FinanceAccountRepo: Repository<FinanceAccountEntity>;
+  private financeAccountRepo: Repository<FinanceAccountEntity>;
+  private financeAccountUserRepo: Repository<FinanceAccountUserEntity>;
   protected allowedFields: (keyof FinanceAccountEntity)[] = [
     'id',
     'name',
@@ -14,7 +18,10 @@ export class FinanceAccountRepository implements IFinanceAccountRepository {
   ];
 
   private constructor(protected readonly dataSource: DataSource) {
-    this.FinanceAccountRepo = dataSource.getRepository(FinanceAccountEntity);
+    this.financeAccountRepo = dataSource.getRepository(FinanceAccountEntity);
+    this.financeAccountUserRepo = dataSource.getRepository(
+      FinanceAccountUserEntity,
+    );
   }
 
   public static createInstance(
@@ -35,11 +42,39 @@ export class FinanceAccountRepository implements IFinanceAccountRepository {
       user: userId,
       financeAccount: data.id,
     }));
-    const createdEntity = this.FinanceAccountRepo.create({
+    const createdEntity = this.financeAccountRepo.create({
       ...data,
       finaceAccountUser,
     });
 
-    await this.FinanceAccountRepo.save(createdEntity);
+    await this.financeAccountRepo.save(createdEntity);
+  }
+
+  public async addUserInAccount(data: FinanceAccountProps): Promise<void> {
+    const finaceAccountUserList = await this.financeAccountUserRepo
+      .createQueryBuilder('financeAccountUser')
+      .leftJoinAndSelect('financeAccountUser.financeAccount', 'financeAccount')
+      .leftJoinAndSelect('financeAccountUser.user', 'user')
+      .where('financeAccountUser.financeAccount = :accountId', {
+        accountId: data.id,
+      })
+      .select(['financeAccountUser.id'])
+      .addSelect(['financeAccount.id'])
+      .addSelect(['user.id'])
+      .getMany();
+
+    const addedUsers = finaceAccountUserList.map((item) => item.user?.['id']);
+    const users = data.users.filter((user) => !addedUsers.includes(user));
+    if (users.length === 0) return;
+
+    const createdFinaceAccountUsers = users.map((userId) => {
+      return this.financeAccountUserRepo.create({
+        id: randomUUID(),
+        user: userId,
+        financeAccount: data.id,
+      });
+    });
+
+    await this.financeAccountUserRepo.save(createdFinaceAccountUsers);
   }
 }
