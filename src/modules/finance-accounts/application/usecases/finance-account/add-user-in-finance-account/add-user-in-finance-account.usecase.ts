119ad3548bf5c11332958cac6fb22be3bb86399b
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-06 17:22:35 -0300

    feat: AddUserInFinanceAccount.UseCase - implemantation

---
+++ src/modules/finance-accounts/application/usecases/finance-account/add-user-in-finance-account/add-user-in-finance-account.usecase.ts
@@ -0,0 +1,56 @@
+import { BadRequestError, ForbiddenError } from '@shared/domain/errors';
+import { DefaultUseCase } from '@shared/domain/usecases';
+import {
+  FinanceAccountFactory,
+  FinanceAccountProps,
+} from '@finance-accounts/domain/entities';
+import { IFinanceAccountRepository } from '@finance-accounts/domain/repositories';
+import { IUserRepository } from '@users/domain/repositories';
+
+export namespace AddUserInFinanceAccount {
+  export type Input = {
+    accountId: string;
+    userId: string;
+    actionDoneBy: string;
+  };
+
+  export type Output = void;
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(
+      private readonly financeAccountRepository: IFinanceAccountRepository,
+      private readonly userRepository: IUserRepository,
+    ) {}
+
+    public async execute(input: Input): Promise<Output> {
+      const { accountId, actionDoneBy, userId } = input;
+      const newUserToAddExists = await this.userRepository.findById(userId, [
+        'id',
+      ]);
+      if (!newUserToAddExists) {
+        throw new BadRequestError('User do not exists');
+      }
+
+      const financeAccountToAddNewUser =
+        await this.financeAccountRepository.findById(accountId);
+      if (!financeAccountToAddNewUser) {
+        throw new BadRequestError('Finance account do not exists');
+      }
+
+      const financeAccount = FinanceAccountFactory.create(
+        financeAccountToAddNewUser as FinanceAccountProps,
+      );
+
+      const userAddingNewUserDoNotBelongToAccount =
+        !financeAccount.checkIfUserIsAlreadyAdded(actionDoneBy);
+      if (userAddingNewUserDoNotBelongToAccount) {
+        throw new ForbiddenError('Action not allowed');
+      }
+
+      financeAccount.addUser(userId);
+      await this.financeAccountRepository.addUserInAccount(
+        financeAccount.toJSON(),
+      );
+    }
+  }
+}
