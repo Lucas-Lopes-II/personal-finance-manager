6e8c469bce0187a7a043e40711c1682e7cf88d77
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-10 11:13:08 -0300

    feat: AuthUseCasesFactory - becomeAdminUser

--- src/modules/auth/application/usecases/auth.usecases.factory.ts
+++ src/modules/auth/application/usecases/auth.usecases.factory.ts
@@ -1,21 +1,29 @@
-import { GenerateSigninToken, Signin } from '@auth/application/usecases';
-import { DefaultUseCase } from '@shared/domain/usecases';
+import {
+  BecomeAdminUser,
+  GenerateSigninToken,
+  Signin,
+} from '@auth/application/usecases';
 import { IUserRepository } from '@users/domain/repositories';
-import { IHasher } from '@shared/domain/crypto';
 import { userRepositoryFactory } from '@users/infra/data/repositories';
+import { IHasher } from '@shared/domain/crypto';
+import { DefaultUseCase } from '@shared/domain/usecases';
 import { hasherFactory } from '@shared/infra/crypto/hasher';
 import { IJsonWebToken, JwtFactory } from '@shared/infra/jwt';
 
 export class AuthUseCasesFactory {
-  public static readonly repo: IUserRepository = userRepositoryFactory();
+  public static readonly userRepo: IUserRepository = userRepositoryFactory();
   public static readonly hasher: IHasher = hasherFactory();
   public static readonly jsonWebToken: IJsonWebToken = JwtFactory.create();
 
   public static signin(): DefaultUseCase {
-    return new Signin.UseCase(this.repo, this.hasher);
+    return new Signin.UseCase(this.userRepo, this.hasher);
   }
 
   public static generateSigninToken(): DefaultUseCase {
     return new GenerateSigninToken.UseCase(this.jsonWebToken);
+  }
+
+  public static becomeAdminUser(): DefaultUseCase {
+    return new BecomeAdminUser.UseCase(this.userRepo);
   }
 }
