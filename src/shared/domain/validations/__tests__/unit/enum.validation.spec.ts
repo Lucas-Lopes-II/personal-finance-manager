5725f54a8d95f904818c6e8144cf934b7717ff39
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-12 19:00:45 -0300

    test: EnumValidation - unit tests

---
+++ src/shared/domain/validations/__tests__/unit/enum.validation.spec.ts
@@ -0,0 +1,41 @@
+import { Validation } from '@shared/domain/validations';
+import { BadRequestError } from '@shared/domain/errors';
+import { Month } from '@shared/domain/enums';
+import { EnumValidation } from '../../enum.validation';
+
+describe('EnumValidation unit tests', () => {
+  let sut: Validation;
+  const dataValidate = {
+    month: Month.AUGUST,
+  };
+
+  beforeEach(() => {
+    sut = new EnumValidation('month', Month, 'Month');
+  });
+
+  it('should vailidate correctly', async () => {
+    expect(() => sut.validate(dataValidate)).not.toThrow();
+  });
+
+  it('should throw a BadRequestError when the given month is not provided', async () => {
+    dataValidate.month = null;
+    expect(() => sut.validate(dataValidate)).toThrow(
+      new BadRequestError(`month is required`),
+    );
+  });
+
+  it('should not throw a BadRequestError when the given month is not provided and isRequired flag is false', async () => {
+    sut = new EnumValidation('month', Month, 'Month', false);
+    dataValidate.month = null;
+
+    expect(() => sut.validate(dataValidate)).not.toThrow();
+  });
+
+  it('should throw a BadRequestError when the given month is not a number', async () => {
+    dataValidate.month = 15 as any;
+
+    expect(() => sut.validate(dataValidate)).toThrow(
+      new BadRequestError(`month should be type enum Month`),
+    );
+  });
+});
