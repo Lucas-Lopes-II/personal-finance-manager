c0d19dfec50a114f588b60b964c3212bfe59fb6d
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-16 11:28:00 -0300

    feat: MonthlyEntryReportUsecaseFactory - implemantation

---
+++ src/modules/entries/application/usecases/monthly-entry-report/monthly-entry-report.usecase.factory.ts
@@ -0,0 +1,20 @@
+import { CreateMothlyEntryReport } from '@entries/application/usecases';
+import { MonthlyEntryReportDataGetwayFactory } from '@entries/infra/data/getways';
+import { MonthlyEntryReportRepositoryFactory } from '@entries/infra/data/repositories';
+import { FinanceAccountFacadeFactory } from '@finance-accounts/infra/facades/finance-account/finance-account.facade.factory';
+
+export class MonthlyEntryReportUsecaseFactory {
+  private static monthlyEntryReportRepo =
+    MonthlyEntryReportRepositoryFactory.create();
+  private static monthlyEntryReportGetway =
+    MonthlyEntryReportDataGetwayFactory.create();
+  private static financeAccountFacade = FinanceAccountFacadeFactory.create();
+
+  public static CreateMothlyEntryReport(): CreateMothlyEntryReport.UseCase {
+    return new CreateMothlyEntryReport.UseCase(
+      this.monthlyEntryReportRepo,
+      this.monthlyEntryReportGetway,
+      this.financeAccountFacade,
+    );
+  }
+}
