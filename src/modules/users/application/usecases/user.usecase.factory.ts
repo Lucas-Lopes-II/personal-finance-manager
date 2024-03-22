50583124a7dda91ecedcaae02d7b77a7b0e4ecab
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-22 17:44:37 -0300

    feat: UserUseCasesFactory - becomeAdmin method

--- src/modules/users/application/usecases/user.usecase.factory.ts
+++ src/modules/users/application/usecases/user.usecase.factory.ts
@@ -1,11 +1,15 @@
 import { IHasher } from '@shared/domain/crypto';
 import { hasherFactory } from '@shared/infra/crypto/hasher';
-import { CreateUser, FindUserById } from '@users/application/usecases';
-import { IUserRepository } from '@users/domain/repositories';
 import {
   IUserDataGetway,
   UserDataGetwayFactory,
 } from '@users/infra/data/getways';
+import {
+  BecomeAdmin,
+  CreateUser,
+  FindUserById,
+} from '@users/application/usecases';
+import { IUserRepository } from '@users/domain/repositories';
 import { userRepositoryFactory } from '@users/infra/data/repositories';
 
 export class UserUseCasesFactory {
@@ -20,5 +24,9 @@ export class UserUseCasesFactory {
 
   public static findUserById(): FindUserById.UseCase {
     return new FindUserById.UseCase(this.getway);
+  }
+
+  public static becomeAdmin(): BecomeAdmin.UseCase {
+    return new BecomeAdmin.UseCase(this.repo);
   }
 }
