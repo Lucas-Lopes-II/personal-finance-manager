17ebe2217e2c73c7416b7c35eac740a9b90ba1a6
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-23 10:46:18 -0300

    test: UserDataGetway - findByEmail method integration tests

--- src/modules/users/infra/data/getways/user.data.getway.test.ts
+++ src/modules/users/infra/data/getways/user.data.getway.test.ts
@@ -63,4 +63,27 @@ describe('UserDataGetway integration tests', () => {
       expect(result.password).toBeUndefined();
     });
   });
+
+  describe('findByEmail', () => {
+    it(`should find an user by email`, async () => {
+      await userRepo.save(data);
+      const result = await sut.findByEmail(data.email);
+
+      expect(result.id).toStrictEqual(data.id);
+      expect(result.name).toStrictEqual('Name');
+      expect(result.email).toStrictEqual('email@example.com');
+    });
+
+    it(`should find an user by email with select fields`, async () => {
+      await userRepo.save(data);
+      const result = await sut.findByEmail(data.email, ['name', 'email']);
+
+      expect(result).toEqual({
+        name: 'Name',
+        email: 'email@example.com',
+      });
+      expect(result.id).toBeUndefined();
+      expect(result.password).toBeUndefined();
+    });
+  });
 });
