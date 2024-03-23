489ab579dab453befe1e21505d12c92ad4a7c101
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-23 10:49:39 -0300

    feat: FindUserByEmail.UseCase -implemantation

---
+++ src/modules/users/application/usecases/find-user-by-email/find-user-by-email.usecase.ts
@@ -0,0 +1,42 @@
+import { NotFoundError } from '@shared/domain/errors';
+import { DefaultUseCase } from '@shared/application/usecases';
+import { EmailValidation, Validation } from '@shared/domain/validations';
+import { IUserDataGetway } from '@users/infra/data/getways';
+
+export namespace FindUserByEmail {
+  export type Input = {
+    email: string;
+    selectedfields?: (keyof Output)[];
+  };
+
+  export type Output = {
+    id?: string;
+    name?: string;
+    email?: string;
+    isAdmin?: boolean;
+  };
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly userGetway: IUserDataGetway) {}
+
+    public async execute(input: Input): Promise<Output> {
+      const validator: Validation<Input> = new EmailValidation('email');
+      validator.validate(input);
+
+      const user = await this.userGetway.findByEmail(
+        input.email,
+        input.selectedfields,
+      );
+      if (!user) {
+        throw new NotFoundError('user not found');
+      }
+
+      return {
+        id: user?.id || undefined,
+        name: user?.name || undefined,
+        email: user?.email || undefined,
+        isAdmin: typeof user?.isAdmin === 'boolean' ? user?.isAdmin : undefined,
+      };
+    }
+  }
+}
--- src/modules/users/application/usecases/index.ts
+++ src/modules/users/application/usecases/index.ts
@@ -1,5 +1,6 @@
 export * from './create-user/create-user.usecase';
 export * from './user.usecase.factory';
 export * from './find-user-by-id/find-user-by-id.usecase';
+export * from './find-user-by-email/find-user-by-email.usecase';
 export * from './become-admin-user/become-admin.usecase';
 // export * from './';
