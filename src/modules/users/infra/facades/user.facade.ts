d21a85a3457600e4b8fd6a2a5d37f86ac7b6b326
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-23 09:59:27 -0300

    feat: UserFacade - becomeAdmin method implemantation

--- src/modules/users/infra/facades/user.facade.ts
+++ src/modules/users/infra/facades/user.facade.ts
@@ -1,25 +1,46 @@
 import {
+  BecomeAdminInput,
+  BecomeAdminOutput,
   FindUserByIdInput,
   FindUserByIdOutput,
   IUserFacade,
 } from '@users/infra/facades';
-import { FindUserById } from '@users/application/usecases';
+import { BecomeAdmin, FindUserById } from '@users/application/usecases';
 
-export type UserFacadeDeps = {
+export type UserFacadeDependencies = {
   findByIdUsecase: FindUserById.UseCase;
+  becomeAdminUsecase: BecomeAdmin.UseCase;
 };
 
 export class UserFacade implements IUserFacade {
-  constructor(private readonly deps: UserFacadeDeps) {}
+  constructor(private readonly dependencies: UserFacadeDependencies) {}
 
   public async findById(
     input: FindUserByIdInput,
   ): Promise<FindUserByIdOutput | Partial<FindUserByIdOutput>> {
-    const data = await this.deps.findByIdUsecase.execute({
+    const { findByIdUsecase } = this.dependencies;
+    const data = await findByIdUsecase.execute({
       id: input.id,
       selectedfields: input.selectedfields as any,
     });
 
     return data;
+  }
+
+  public async becomeAdmin(
+    input: BecomeAdminInput,
+  ): Promise<BecomeAdminOutput> {
+    const { becomeAdminUsecase } = this.dependencies;
+    const data = await becomeAdminUsecase.execute({
+      userId: input.userId,
+      actionDoneBy: input.actionDoneBy,
+    });
+
+    return {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      isAdmin: data.isAdmin,
+    };
   }
 }
